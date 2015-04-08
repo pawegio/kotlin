b7a077e9512b89653e182e1a1ab254387b4fdfd6
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-04-08 12:18:11 +0300

    Pattern API

--- libraries/stdlib/src/kotlin/text/regex/Pattern.kt
+++ libraries/stdlib/src/kotlin/text/regex/Pattern.kt
@@ -27,15 +27,37 @@ public trait FlagEnum {
     public val value: Int
     public val mask: Int
 }
+public fun Iterable<FlagEnum>.toInt(): Int =
+        this.fold(0, { value, option -> value or option.value })
+public fun <T: FlagEnum> fromInt(value: Int, allValues: Array<T>): Set<T> =
+        allValues.filter({ value and it.mask == it.value }).toSet()
+
 
 public enum class PatternOptions(override val value: Int, override val mask: Int = value) : FlagEnum {
-    IGNORE_CASE : PatternOptions(0x01)
-    MULTILINE : PatternOptions(0x02)
-    // ... other options
+    // common
+    IGNORE_CASE : PatternOptions(NativePattern.CASE_INSENSITIVE)
+    MULTILINE : PatternOptions(NativePattern.MULTILINE)
 
-
+    //jvm-specific
+    LITERAL : PatternOptions(NativePattern.LITERAL)
+    UNICODE_CASE: PatternOptions(NativePattern.UNICODE_CASE)
+    UNIX_LINES: PatternOptions(NativePattern.UNIX_LINES)
+    COMMENTS: PatternOptions(NativePattern.COMMENTS)
+    DOTALL: PatternOptions(NativePattern.DOTALL)
+    CANON_EQ: PatternOptions(NativePattern.CANON_EQ)
 }
 
+/* in JS
+public enum class PatternOptions(val value: String) {
+    IGNORE_CASE : PatternOptions("i")
+    MULTILINE : PatternOptions("m")
+}
+*/
+
+public trait MatchGroup {
+    public val range: IntRange
+    public val value: String
+}
 
 public trait MatchResult : MatchGroup {
     public val groups: List<MatchGroup?>
@@ -43,54 +65,42 @@ public trait MatchResult : MatchGroup {
 }
 
 
-public trait MatchGroup {
-    public val range: IntRange
-    public val value: String
-}
+public class Pattern ( /* visibility? */ val nativePattern: NativePattern) {
 
-
-
-public class Pattern /* internal */ ( /* internal */ val nativePattern: NativePattern) {
-
-    public constructor(pattern: String, options: Set<PatternOptions>): this(NativePattern.compile(pattern))
+    public constructor(pattern: String, options: Set<PatternOptions>): this(NativePattern.compile(pattern, options.toInt()))
     public constructor(pattern: String, vararg options: PatternOptions) : this(pattern, options.toSet())
 
     public val pattern: String
         get() = nativePattern.pattern()
 
-    public val options: Set<PatternOptions>
-        get() = TODO
+    public val options: Set<PatternOptions> = fromInt(nativePattern.flags(), PatternOptions.values())
 
+    public fun matches(input: CharSequence): Boolean = nativePattern.matcher(input).matches()
 
-    public fun matches(string: CharSequence): Boolean = nativePattern.matcher(string).matches()
+    public fun match(input: CharSequence): MatchResult? = nativePattern.matcher(input).findNext(0)
 
-    public fun match(string: CharSequence): MatchResult? {
-        val matcher = nativePattern.matcher(string)
-        return if (matcher.matches())
-            matcher.findNext()
-        else
-            null
+    public fun matchAll(input: CharSequence): Sequence<MatchResult> = sequence({ match(input) }, { match -> match.next() })
+
+    public fun replace(input: CharSequence, replacement: String): String = nativePattern.matcher(input).replaceAll(replacement)
+    public fun replace(input: CharSequence, evaluator: (MatchResult) -> String): String = TODO
+
+    public fun split(input: CharSequence, limit: Int = 0): List<String> = nativePattern.split(input, limit).asList() // TODO: require(limit>=0)
+
+    companion object {
+        public fun fromLiteral(literal: String): Pattern = Pattern(literal, PatternOptions.LITERAL)
+        public fun escape(literal: String): String = NativePattern.quote(literal)
+        public fun escapeReplacement(literal: String): String = Matcher.quoteReplacement(literal)
     }
 
-    public fun matchAll(string: CharSequence): Sequence<MatchResult> = sequence({ match(string) }, { match -> match.next() })
-
-    public fun replace(string: CharSequence, replacement: String): String = nativePattern.matcher(string).replaceAll(replacement)
-    public fun replace(string: CharSequence, evaluator: (MatchResult) -> String): String = TODO
-
-    public fun split(string: CharSequence, limit: Int = 0): List<String> = nativePattern.split(string, limit).asList()
-
-    companion object {}
 }
 
-
-public fun Pattern.Companion.escape(literal: String): String = NativePattern.quote(literal)
-public fun Pattern.Companion.escapeReplacement(literal: String): String = Matcher.quoteReplacement(literal)
-
+public fun String.toPattern(vararg options: PatternOptions): Pattern = Pattern(this, *options)
+public fun String.toPattern(options: Set<PatternOptions>): Pattern = Pattern(this, options)
 
 
 
-private fun Matcher.findNext(): MatchResult? {
-    if (!find())
+private fun Matcher.findNext(from: Int): MatchResult? {
+    if (!find(from))
         return null
 
     var matchResult = this as java.util.regex.MatchResult
@@ -120,7 +130,7 @@ private fun Matcher.findNext(): MatchResult? {
 
         override fun next(): MatchResult? {
             matchResult = this@findNext.toMatchResult()
-            return this@findNext.findNext() // TODO: advance next
+            return this@findNext.findNext(matchResult.end()) // TODO: advance next
         }
     }
 }
